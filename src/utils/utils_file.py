4aa9b0760bf0293633d7ebec8b3a20f33190ef54
Bartłomiej Fydrych <fydbar@gmail.com>
2026-08-19 13:59:00 +0200

    2026.08.19 – Dodaj pliki JSON z oczekiwanymi response i ich obsługę

--- src/utils/utils_file.py
+++ src/utils/utils_file.py
@@ -10,6 +10,7 @@ from exceptions.exception_resource_read import ExceptionResourceRead
 _SRC_DIR = Path(__file__).resolve().parent.parent
 
 _RESOURCES_DIR: Path = _SRC_DIR / "resources"
+_EXPECTED_RESPONSES_DIR: Path = _SRC_DIR / "expected_responses"
 
 
 # ==========================================================================================================
@@ -17,35 +18,32 @@ _RESOURCES_DIR: Path = _SRC_DIR / "resources"
 # ==========================================================================================================
 
 # EXAMPLE OF USE:
-# expected_response_json_file: str = read_resource_file_as_string(
-#     "tests/expected_responses/emoji/GET_ListAvailableEmojiExpected/P4_ExpectedGetListAvailableEmojiResponse.json"
-# )
+# body: str = read_resource_file_as_string("payloads/create-board.json")
 
 def read_resource_file_as_string(resource_path: str) -> str:
-    file_path: Path = _RESOURCES_DIR / resource_path
+    return _read_file_as_string(_RESOURCES_DIR, resource_path)
+
+
+# EXAMPLE OF USE:
+# expected_response_json_file: str = read_expected_response_file_as_string(
+#     "emoji/GET_list_available_emoji/P4_ExpectedGetListAvailableEmojiResponse.json"
+# )
+
+def read_expected_response_file_as_string(expected_response_path: str) -> str:
+    return _read_file_as_string(_EXPECTED_RESPONSES_DIR, expected_response_path)
+
+
+# ==========================================================================================================
+# METHODS – PRIVATE
+# ==========================================================================================================
+
+def _read_file_as_string(base_dir: Path, relative_path: str) -> str:
+    file_path: Path = base_dir / relative_path
 
     if not file_path.is_file():
-        raise FileNotFoundError(f"Resource not found: {resource_path}")
+        raise FileNotFoundError(f"File not found: {relative_path}")
 
     try:
         return file_path.read_text(encoding="utf-8")
     except OSError as e:
-        raise ExceptionResourceRead(f"Failed to read resource: {resource_path}") from e
-
-
-"""
-########################################################################################################################
-MY ADDITIONAL NOTES
-########################################################################################################################
-
------------------------------
-BONUS – REQUESTS USE-CASE
------------------------------
-
-body: str = read_resource_file_as_string("payloads/create-user.json").replace(
-    "${username}", faker.name().username()
-)
-
-response: Response = session.post("/users", data=body)
-assert response.status_code == 201
-"""
+        raise ExceptionResourceRead(f"Failed to read file: {relative_path}") from e
